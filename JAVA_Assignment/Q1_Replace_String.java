91822aadb1ab5b7aaf933e6198c83b881d888bf3
siddhant1098 <60687682+siddhant1098@users.noreply.github.com>
2020-02-19 01:06:37 +0530

    Delete Q1_Replace_String.java

--- JAVA_Assignment/Q1_Replace_String.java
+++
@@ -1,9 +0,0 @@
-public class Q1_Replace_String {
-    public  static void main(String[] args){
-        String Str = new String("Welcome to ToTheNew This is a class of JAVA_Programming");
-        System.out.println("Original String is :-"+ Str);
-        System.out.println("Replacing a substring inside a string with other string ");
-        System.out.println("New Updated String is:- ");
-        System.out.println(Str.replace("ToTheNew", "Company of Big Data"));
-    }
-}
