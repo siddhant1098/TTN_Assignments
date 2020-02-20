3cba0baf7bf78710ca5c17bb795c99df91b99db2
siddhant1098 <60687682+siddhant1098@users.noreply.github.com>
2020-02-20 17:27:55 +0530

    Delete Q3_Occurance_of_Character_withoutLoop.java

--- JAVA_Assignment/Q3_Occurance_of_Character_withoutLoop.java
+++
@@ -1,9 +0,0 @@
-public class Q3_Occurance_of_Character_withoutLoop{
-    public static void main(String[] args)
-    {
-        String str = "Big black bug bit a big black dog on his big black nose";
-        int count = str.length() - str.replace("b", "").length();
-        System.out.println("Number of occurances of 'b' in "+" = "+count);
-    }
-}
-
